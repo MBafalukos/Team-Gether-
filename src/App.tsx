ceb8b52fdfcf741c81ec29ae2e1c59641f58c71c
Argyris <vassilopargiris@gmail.com>
2024-09-27 12:09:34 +0300

    pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Engagement from "./pages/Engagment";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndustriesPage from "./pages/IndustriesPage";
 import AboutPage from "./pages/AboutPage";
+import PricingPage from "./pages/PricingPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/industries" element={<IndustriesPage />} />
           <Route path="/engagment" element={<Engagement />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/pricing" element={<PricingPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Pricing.tsx
+++ src/Pricing.tsx
@@ -1,26 +1,27 @@
-import PricingCards2  from './PricingCards2'
-import { plans } from './data'
+import PricingCards2 from "./PricingCards2";
+import { plans } from "./data";
+import Prices from "./assets/price-list.svg";
 
-const PricingPlans = () => {
-	return (
-		<section className="py-20">
-			<div className="container">
-				<div className="text-center max-w-2xl mx-auto">
-					<span className="text-sm font-medium py-1 px-3 rounded-full text-primary bg-primary/10">
-						Plans
-					</span>
-					<h2 className="md:text-3xl/tight text-3xl font-semibold my-4">
-						Pricing Plans
-					</h2>
-					<p className="md:text-lg text-slate-500 leading-7">
-						Pricing that <span className="text-primary">works</span> for
-						everyone.
-					</p>
-				</div>
-                <PricingCards2 plans={plans} />
-			</div>
-		</section>
-	)
-}
+const Pricing = () => {
+  return (
+    <section className="py-20 px-12">
+      <div className="container">
+        <div className="text-center max-w-2xl mx-auto">
+          <div className="flex justify-center items-center">
+            <img src={Prices} className="h-12 w-12" />
+            <h2 className="md:text-3xl/tight text-3xl font-semibold my-4">
+              &nbsp;Pricing Plans
+            </h2>
+          </div>
+          <p className="md:text-lg text-slate-500 leading-7">
+            Pricing that <span className="text-primary">works</span> for
+            everyone.
+          </p>
+        </div>
+        <PricingCards2 plans={plans} />
+      </div>
+    </section>
+  );
+};
 
-export default PricingPlans
+export default Pricing;
